142c6da646fcf0237cdd4d1f7401c9f7b428ae9b
Max Skrypnyk <moxskr@gmail.com>
2020-01-11 14:38:51 +0200

    added footer

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,22 @@ import PhotoList from "./PhotoList";
 
 const App = () => {
     return(
-        <div className="container-fluid">
-            <div className="row header">
-                <div className="col-12 text-center">
-                    <h1>TEST APP</h1>
+        <div className="container-fluid d-flex flex-column min">
+            <div className="content flex-grow-1">
+                <header className="row header">
+                    <div className="col-12 text-center">
+                        <h1>TEST APP</h1>
+                    </div>
+                </header>
+                <div className="row photo-list ">
+                    <PhotoList/>
                 </div>
             </div>
-            <div className="row photo-list">
-                <PhotoList/>
-            </div>
+            <footer className="footer">
+                <div className="footer-content">
+                    © 2018-2019
+                </div>
+            </footer>
         </div>
     )
 };
